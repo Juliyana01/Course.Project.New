c2ead3c1c273d775122372ede450d06aa420ed02
Juliyana <juliyana.pavlova01@gmail.com>
2022-02-05 17:32:47 +0200

    Course Project

--- src/test/java/course/project/FirstTest.java
+++ src/test/java/course/project/FirstTest.java
@@ -1,4 +1,31 @@
 package course.project;
+ import io.github.bonigarcia.wdm.WebDriverManager;
+ import openqa.selenium.WebDriver;
+ import org.openqa.selenium.By;
+ import org.openqa.selenium.WebDriver;
+ import org.openqa.selenium.WebElement;
+ import org.openqa.selenium.chrome.ChromeDriver;
+ import org.testing.anotation.BeforeTest;
+ import org.testng.annotations.BeforeTest;
+ import org.testng.annotations.Test;
+
 
 public class FirstTest {
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setUp (){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+    }
+
+    @Test
+    public void login(){
+        driver.get("https://www.saucedemo.com/");
+
+        WebElement userNameInput = driver.findElement(By.id("user-name"));
+        userNameInput.sendKeys("standard_user");
+    }
+
 }
